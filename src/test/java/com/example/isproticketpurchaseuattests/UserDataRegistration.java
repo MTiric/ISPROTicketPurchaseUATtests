f701e48e21fd94fb34b4b4c91191aef61e26b5b9
MTiric <matko.tiric@king-ict.hr>
2023-09-05 13:48:26 +0200

    Updated sorting of EnvironmentData - updated test result status message
    Updated bug where old mail was set

--- src/test/java/com/example/isproticketpurchaseuattests/UserDataRegistration.java
+++ src/test/java/com/example/isproticketpurchaseuattests/UserDataRegistration.java
@@ -1,13 +1,12 @@
 package com.example.isproticketpurchaseuattests;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class UserDataRegistration {
 
     public static void fillInfo(String user, String pass){
-        $x("//input[@id='fUsername']").sendKeys("matko.tiric@king-ict.hr");
-        $x("//input[@id='fPassword']").sendKeys("KingMT329!");
+        $x("//input[@id='fUsername']").sendKeys(user);
+        $x("//input[@id='fPassword']").sendKeys(pass);
         $x("//button[@id='korisnikSubmitButton']").click();
     }
 }
